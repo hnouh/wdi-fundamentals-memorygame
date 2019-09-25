8f16449be056bd3bef7678bf266b0e46ddf1816a
hnooh <30382036+hnooh@users.noreply.github.com>
2019-09-25 17:21:59 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,24 +23,23 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-var cardOne;
-
-cardOne = cards[0];
-cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-/////////////////////////////////////////////////
-
-if ( cardsInPlay.length === 2){
+function checkForMatch(){
   if (cardsInPlay[0] === cardsInPlay[1]) {
-  alert("You found a match!");
+    console.log("You found a match!");
+  } else {
+    console.log("Sorry, try again.");
+  }
 }
-  else{
-  alert("Sorry, try again.");
+
+function flipCard(cardId){
+
+cardsInPlay.push(cards[cardId]);
+
+console.log("User flipped" + cards[cardId]);
+    if ( cardsInPlay.length === 2){
+      checkForMatch();
+    }
 }
-}
+
+flipCard(0);
+flipCard(1);
